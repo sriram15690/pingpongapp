06e1fcfc3bc0c0148df8aefbfbf24dfa8121ba41
Sriram R <sriram15690@mail.com>
2015-09-14 20:53:27 +0530

    Adding Validations to models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,12 @@
 class Booking < ActiveRecord::Base
   belongs_to :user
   belongs_to :table
+  validates :user, presence: true
+  validates :table, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :duration, :numericality => { :greater_than_or_equal_to => 10, :less_than_or_equal_to => 60, :message => "Duration Should be Betweem 10 & 60 minutes"}
+  validate  :minimum_time_gap_for_user
+  validate :overlapping_booking_check
+
 end
